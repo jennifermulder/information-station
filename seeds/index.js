98a4bd652872535597c3fcfe7bd04b095369f22a
chulopapi <marlon.guandique@gmail.com>
2020-08-30 10:20:27 -0700

    added dahsboard, login, edit, logout and single-post handlebars

--- seeds/index.js
+++ seeds/index.js
@@ -1,53 +1,8 @@
-// const seedUsers = require('./user-seeds');
-// const seedPosts = require('./post-seeds');
-// const seedComments = require('./comment-seeds');
-// const seedBusinesses = require('./business-seeds');
-// ​
-// const sequelize = require('../config/connection');
-// ​
-// async function seedTablesInOrder() {
-//   await sequelize.sync({ force: true });
-//   console.log('--------------');
-//   await seedUsers();
-//   console.log('--------------');
-// ​
-//   await seedPosts();
-//   console.log('--------------');
-// ​
-//   await seedComments();
-//   console.log('--------------');
-// ​
-//   await seedBusinesses();
-//   console.log('--------------');
-// ​
-//   process.exit(0);
-// };
-// ​
-// seedTablesInOrder();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Message Luigi
-
+const sequelize = require('../config/connection');
+const seedUsers = require('./user-seeds');
+ console.log('LINE 2')
+ // const seedPosts = require('./post-seeds');
+ // const seedComments = require('./comment-seeds');
+ // const seedBusinesses = require('./business-seeds');
+ // ​
+console.log('LINE 3')
